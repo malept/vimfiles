d08194742483606520dff6abdc1d44e5aa77b76d
Mark Lee <vim@lazymalevolence.com>
2018-04-30 23:16:33 -0700

    Fix submodule updating

--- update_bundles.sh
+++ update_bundles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git submodule foreach 'git fetch --recurse-submodules origin'
-git submodule update --remote --recursive
+git submodule update --recursive
 git add vim/bundle
 git add special
 git commit -m "Update all bundles"
