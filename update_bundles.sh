a1bfeaabf5335e3b344389813d4c2e2971e153b3
Mark Lee <vim@lazymalevolence.com>
2018-05-31 14:56:59 -0700

    Better bundle updating now that git has better submodule commands

--- update_bundles.sh
+++ update_bundles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git submodule foreach 'git fetch --recurse-submodules origin'
-git submodule update --recursive
+git submodule update --remote
+git submodule update --recursive --init
 git add vim/bundle
 git add special
 git commit -m "Update all bundles"
