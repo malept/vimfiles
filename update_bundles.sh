241d1e9d3f395484dabe19cbf697a5e7b6a754da
Mark Lee <vim@lazymalevolence.com>
2019-01-04 22:21:50 -0800

    Another fix to the update bundles script

--- update_bundles.sh
+++ update_bundles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git submodule update --remote --recursive
-git submodule update --recursive --init
 git add vim/bundle
 git add special
 git commit -m "Update all bundles"
+git submodule update --recursive --init
