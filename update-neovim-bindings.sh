a9591caa6aed2ee88373188e273c7e204a7fe557
Mark Lee <vim@lazymalevolence.com>
2019-07-06 00:11:58 -0700

    Update neovim bindings script to update the proper package

--- update-neovim-bindings.sh
+++ update-neovim-bindings.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(source ~/.local/share/virtualenv/neovim/bin/activate && pip install -U neovim)
-(source ~/.local/share/virtualenv/neovim3/bin/activate && pip install -U neovim)
+(source ~/.local/share/virtualenv/neovim/bin/activate && pip install -U pynvim)
+(source ~/.local/share/virtualenv/neovim3/bin/activate && pip install -U pynvim)
