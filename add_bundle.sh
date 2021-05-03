4ca7e76d03046402596690e8ab60711e69dca34e
Mark Lee <vim@lazymalevolence.com>
2021-05-03 14:25:34 -0700

    chore: allow add_bundle.sh to add multiple bundles at once

--- add_bundle.sh
+++ add_bundle.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-URL="$1"
-shift
-
-if test -z "$URL"; then
-    echo "Usage: $0 URL_TO_GIT_REPO" 1>&2
-    exit 1
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 URL_TO_GIT_REPO […]" 1>&2
+  exit 1
 fi
 
-bundle=$(basename "$URL" .git)
-commit_msg="Add the $bundle bundle"
+# From: https://stackoverflow.com/a/17841619
+function join_by { local IFS="$1"; shift; echo "$*"; }
 
-if grep -qF "s:plugify('$bundle'" vimrc.d/001-plug.vim; then
-    git add vimrc.d/001-plug.vim
-else
+bundles=()
+
+for URL in "$@"; do
+  bundle="$(basename "$URL" .git)"
+
+  if ! grep -qF "s:plugify('$bundle'" vimrc.d/001-plug.vim; then
     echo "Add the new bundle to vimrc.d/001-plug.vim before calling this script" 1>&2
     exit 1
-fi
+  fi
 
-git submodule add "$@" -- "$URL" vim/bundle/$bundle
+  bundles+=("$bundle")
+  git submodule add -- "$URL" vim/bundle/$bundle
+done
+
+commit_msg="feat: add bundles ($(join_by , "${bundles[@]}"))"
+
+git add vimrc.d/001-plug.vim
 git commit -m "$commit_msg"
