80a375f7feef6cbef6b294b443b12e11aa27318d
Mark Lee <vim@lazymalevolence.com>
2021-10-18 01:15:25 -0700

    feat(lsp): add eslint language server support

--- lua/lspcompletion.lua
+++ lua/lspcompletion.lua
@@ -6,7 +6,7 @@ cmp.setup({
 })
 
 local lsp = require'lspconfig'
-local lang_servers = {'cssls', 'gopls', 'html', 'jsonls', 'rust_analyzer', 'solargraph', 'tsserver', 'vimls'}
+local lang_servers = {'cssls', 'eslint', 'gopls', 'html', 'jsonls', 'rust_analyzer', 'solargraph', 'tsserver', 'vimls'}
 for _, ls in ipairs(lang_servers) do
   lsp[ls].setup {
     capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
