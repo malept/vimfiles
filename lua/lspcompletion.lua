bfc9a9bf8fd396be85b62ae41091af545a2befbd
Mark Lee <vim@lazymalevolence.com>
2021-10-18 01:16:04 -0700

    fix(nvim-cmp): add key mappings for LSP completion

--- lua/lspcompletion.lua
+++ lua/lspcompletion.lua
@@ -2,7 +2,14 @@ local cmp = require'cmp'
 cmp.setup({
   sources = {
     { name = 'nvim_lsp' },
-  }
+  },
+  mapping = {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
 })
 
 local lsp = require'lspconfig'
