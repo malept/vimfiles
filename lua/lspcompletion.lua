f5f3aeb1338e2a9eeec5ebc038e9549efed4c3cb
Mark Lee <vim@lazymalevolence.com>
2021-11-21 18:22:48 -0800

    fix(lspcompletion): re-enable <CR> for nvim-cmp based on recommendations

--- lua/lspcompletion.lua
+++ lua/lspcompletion.lua
@@ -8,8 +8,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    -- Disabled because of autopairs
-    -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
   },
 })
 
