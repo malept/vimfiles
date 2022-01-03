f5034c42fcba0c46296f5c299f7588194e12eb6a
Mark Lee <vim@lazymalevolence.com>
2022-01-03 11:41:05 -0800

    feat(lsp): add pyright

--- lua/lspcompletion.lua
+++ lua/lspcompletion.lua
@@ -13,9 +13,10 @@ cmp.setup({
 })
 
 local lsp = require'lspconfig'
-local lang_servers = {'cssls', 'eslint', 'gopls', 'html', 'jsonls', 'rust_analyzer', 'solargraph', 'tsserver', 'vimls'}
+local lang_servers = {'cssls', 'eslint', 'gopls', 'html', 'jsonls', 'pyright', 'rust_analyzer', 'solargraph', 'tsserver', 'vimls'}
 for _, ls in ipairs(lang_servers) do
   lsp[ls].setup {
     capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   }
 end
+
