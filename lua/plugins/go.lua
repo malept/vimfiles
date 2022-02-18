2ced060928899bb46c8ca936eaee539673e3b7da
Mark Lee <vim@lazymalevolence.com>
2022-02-17 18:48:30 -0800

    fix(go): only setup if gopls exists

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,10 +1,12 @@
 -- go.nvim
-require('go').setup({
-  goimport = 'gopls', -- if set to 'gopls' will use golsp format
-  gofmt = 'gopls', -- if set to gopls will use golsp format
-  lsp_cfg = {
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  },
-})
--- Format + Import on save
-vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
+if vim.fn.executable('gopls') == 1 then
+  require('go').setup({
+    goimport = 'gopls', -- if set to 'gopls' will use golsp format
+    gofmt = 'gopls', -- if set to gopls will use golsp format
+    lsp_cfg = {
+      capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    },
+  })
+  -- Format + Import on save
+  vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
+end
