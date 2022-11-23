174267094b7f0dcf3982764b27eac9e692b2723d
Mark Lee <vim@lazymalevolence.com>
2022-11-22 22:56:41 -0800

    fix(lsp): update deprecated syntax

--- lua/lspcompletion.lua
+++ lua/lspcompletion.lua
@@ -16,6 +16,6 @@ local lsp = require'lspconfig'
 local lang_servers = {'cssls', 'eslint', 'gopls', 'html', 'jsonls', 'rust_analyzer', 'solargraph', 'tsserver', 'vimls'}
 for _, ls in ipairs(lang_servers) do
   lsp[ls].setup {
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    capabilities = require('cmp_nvim_lsp').default_capabilities()
   }
 end
