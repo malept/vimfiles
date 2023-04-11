e12f0ae71f360052a0526e2c6d05c19879ebe37d
Mark Lee <vim@lazymalevolence.com>
2023-04-10 17:34:15 -0700

    Update autopairs config

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -6,11 +6,12 @@ local setup = function()
     check_ts = true,
   })
 
+  npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
   npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
 
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
   local cmp = require('cmp')
-  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 end
 
 return {
