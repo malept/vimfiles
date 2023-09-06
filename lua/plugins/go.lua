e37628765eabaffc219b99259e85e4c3ad4e218f
Mark Lee <vim@lazymalevolence.com>
2023-09-05 22:57:03 -0700

    fix(go): use lspconfig for go again

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -2,15 +2,12 @@ local plugin = require('plugin_util')
 
 local setup = function()
   if vim.fn.executable('gopls') == 1 then
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
     require('go').setup({
       goimport = 'gopls', -- if set to 'gopls' will use golsp format
       gofmt = 'gopls', -- if set to gopls will use golsp format
-      lsp_cfg = {
-        capabilities = capabilities,
-      },
+      lsp_cfg = false, -- let lspcompletion.lua handle LSP
     })
-    -- Format + Import on save
+    -- Import on save
     local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "*.go",
--- lua/plugins/lspcompletion.lua
+++ lua/plugins/lspcompletion.lua
@@ -22,7 +22,7 @@ local setup = function()
   })
 
   local lsp = require('lspconfig')
-  local lsp_on_attach = function(client, bufnum)
+  local lsp_set_keymaps = function(bufnum)
     local function buf_set_keymap(binding, lua_fn, desc)
       local opts = { buffer = bufnum, desc = desc }
       vim.keymap.set('n', binding, lua_fn, opts)
@@ -34,17 +34,25 @@ local setup = function()
     buf_set_keymap('gd', function() require('telescope.builtin').lsp_definitions() end, 'Select definition')
     buf_set_keymap('gi', function() require('telescope.builtin').lsp_implementations() end, 'Select implementation')
     buf_set_keymap('gr', function() require('telescope.builtin').lsp_references() end, 'Select reference')
+  end
+  local lsp_on_attach = function(client, bufnum)
+    lsp_set_keymaps(bufnum)
     -- lsp-format
     require('lsp-format').on_attach(client)
   end
+  local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
   local lsp_setup = {
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    capabilities = lsp_capabilities,
     on_attach = lsp_on_attach
   }
   -- gopls is not included here because go.nvim handles it, see lua/plugins/go.lua
   local lang_servers = {'cssls', 'eslint', 'html', 'solargraph', 'tsserver', 'vimls'}
   for _, ls in ipairs(lang_servers) do
     lsp[ls].setup(lsp_setup)
+  end
+
+  if vim.fn.executable('gopls') == 1 then
+    lsp.gopls.setup(lsp_setup)
   end
 
   if vim.fn.executable('pylsp') == 1 then
@@ -54,7 +62,7 @@ local setup = function()
   end
 
   lsp.jsonls.setup {
-      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      capabilities = lsp_capabilities,
       on_attach = lsp_on_attach,
       settings = {
         json = {
