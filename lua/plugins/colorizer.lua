f707b117b70ce817285b015a0568549dbea26005
Mark Lee <malept@users.noreply.github.com>
2023-10-27 22:48:39 -0700

    fix(colorizer): simplify setup

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -4,6 +4,7 @@ return {
   plugin.not_vscode_plugin({
     'NVChad/nvim-colorizer.lua',
     cond = function() return jit ~= nil end,
-    config = function() require('colorizer').setup() end,
+    main = 'colorizer',
+    config = true,
   }),
 }
