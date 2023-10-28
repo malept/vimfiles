710e6e77bfe2e0d9e23b62fded7c6e0ef37bf1c7
Mark Lee <vim@lazymalevolence.com>
2023-10-27 20:28:52 -0700

    fix: upgrade to indent_blankline v3

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,17 +1,14 @@
 local plugin = require('plugin_util')
 
-local setup = function()
-  require('indent_blankline').setup {
+return {
+  plugin.not_vscode_plugin({'lukas-reineke/indent-blankline.nvim',
+    event = { "BufReadPost", "BufNewFile" },
+    main = 'ibl',
+    opts = {
       space_char_blankline = ' ',
       show_current_context = true,
       show_current_context_start = true,
       buftype_exclude = {"terminal"},
-  }
-end
-
-return {
-  plugin.not_vscode_plugin({'lukas-reineke/indent-blankline.nvim',
-    event = { "BufReadPost", "BufNewFile" },
-    config = setup,
+    }
   }),
 }
