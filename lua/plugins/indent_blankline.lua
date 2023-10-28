d76478c3421116f3f01b684bc3013bc44ea9b579
Mark Lee <vim@lazymalevolence.com>
2023-10-27 21:14:31 -0700

    fix: indent_blankline opts no longer necessary

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -4,11 +4,5 @@ return {
   plugin.not_vscode_plugin({'lukas-reineke/indent-blankline.nvim',
     event = { "BufReadPost", "BufNewFile" },
     main = 'ibl',
-    opts = {
-      space_char_blankline = ' ',
-      show_current_context = true,
-      show_current_context_start = true,
-      buftype_exclude = {"terminal"},
-    }
   }),
 }
