ec17ab6b330c85cc5649a60a5d15276fb38c1f44
Mark Lee <vim@lazymalevolence.com>
2023-12-27 18:52:30 -0800

    refactor(go): use cond

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,20 +1,17 @@
 local plugin = require('plugin_util')
 
 local setup = function()
-  if vim.fn.executable('gopls') == 1 then
-    require('go').setup({
-      goimport = 'gopls', -- if set to 'gopls' will use golsp format
-      gofmt = 'gopls', -- if set to gopls will use golsp format
-      lsp_cfg = false, -- let lspcompletion.lua handle LSP
-    })
-    -- Import on save
-    local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*.go",
-      callback = function() require('go.format').goimport() end,
-      group = format_sync_grp,
-    })
-  end
+  require('go').setup({
+    goimport = 'gopls', -- if set to 'gopls' will use golsp format
+    gofmt = 'gopls', -- if set to gopls will use golsp format
+    lsp_cfg = false, -- let lspcompletion.lua handle LSP
+  })
+  -- Import on save
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function() require('go.format').goimport() end,
+    group = vim.api.nvim_create_augroup("GoImport", {}),
+  })
 end
 
 return {
@@ -22,6 +19,7 @@ return {
     -- last release was 2022-08-09
     version = nil,
     branch = 'master',
+    cond = function() return vim.fn.executable('gopls') == 1 end,
     event = 'CmdlineEnter',
     ft = {'go', 'gomod'},
     dependencies = {
