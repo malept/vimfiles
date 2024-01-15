ba8dacae996f753ad55e2106ca2e589654a68e9b
Mark Lee <vim@lazymalevolence.com>
2024-01-15 09:27:54 -0800

    chore: run stylua

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -6,21 +6,29 @@ local setup = function()
   vscode.load_launchjs()
   local debugpy_venv = vim.fn.expand("~/.local/share/virtualenv/debugpy")
   if vim.fn.isdirectory(debugpy_venv) then
-    require('dap-python').setup(debugpy_venv .. "/bin/python")
+    require("dap-python").setup(debugpy_venv .. "/bin/python")
   end
 end
 
 return {
-  plugin.not_vscode_plugin({ "rcarriga/nvim-dap-ui",
+  plugin.not_vscode_plugin({
+    "rcarriga/nvim-dap-ui",
     dependencies = { "mfussenegger/nvim-dap" },
     keys = {
-      { "<leader>du", function() require("dapui").toggle() end, desc = "Dap UI" },
+      {
+        "<leader>du",
+        function()
+          require("dapui").toggle()
+        end,
+        desc = "Dap UI",
+      },
     },
   }),
-  plugin.not_vscode_plugin({ "mfussenegger/nvim-dap",
+  plugin.not_vscode_plugin({
+    "mfussenegger/nvim-dap",
     dependencies = {
       "L3MON4D3/LuaSnip", -- For the jsonc parser
-      "mfussenegger/nvim-dap-python"
+      "mfussenegger/nvim-dap-python",
     },
     config = setup,
   }),
