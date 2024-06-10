748f4be41019592f20d63870cb6fd937768c50df
Mark Lee <vim@lazymalevolence.com>
2024-06-10 16:11:23 -0700

    refactor: simplify some keymap definitions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,12 +3,8 @@ local plugin = require("plugin_util")
 local setup = function()
   require("telescope").load_extension("fzy_native")
 
-  vim.keymap.set("n", "<C-p>", function()
-    require("telescope_project_files").project_files({})
-  end, { silent = true })
-  vim.keymap.set("n", "<Leader>Gb", function()
-    require("telescope.builtin").git_branches({})
-  end, { silent = true })
+  vim.keymap.set("n", "<C-p>", require("telescope_project_files").project_files, { silent = true })
+  vim.keymap.set("n", "<Leader>Gb", require("telescope.builtin").git_branches, { silent = true })
 end
 
 return {
