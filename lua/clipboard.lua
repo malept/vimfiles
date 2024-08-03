36849deec34a6971dbd3d12d4e7a97b10d28002f
Mark Lee <vim@lazymalevolence.com>
2024-08-02 20:48:06 -0700

    fix(clipboard): remove obsolete config

--- lua/clipboard.lua
+++ lua/clipboard.lua
@@ -2,9 +2,3 @@
 for k in pairs({ "y", "Y", "p", "P" }) do
   vim.keymap.set("", string.format("<Leader>%s", k), string.format([["+%s]], k))
 end
-
--- WSL clipboard, see:
--- https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
-if vim.fn.executable("win32yank.exe") == 1 then
-  vim.opt.clipboard = "unnamedplus"
-end
